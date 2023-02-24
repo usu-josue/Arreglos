cb948b6e5a0ee07b19aaeec3a5b33b0fed40cf34
usu-josue <126061303+usu-josue@users.noreply.github.com>
2023-02-24 16:30:45 -0500

    Update Class1.cs

--- Arreglos/Class1.cs
+++ Arreglos/Class1.cs
@@ -12,6 +12,7 @@ namespace Arreglos
         static void Main(string[] args) {
 
             Console.WriteLine("Hola");
+            Console.WriteLine("Hola Josue Hermoso precioso");
         
         }
 
